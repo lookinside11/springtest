2ac66868d8aedcd145b7b7a70c52e133ce5914b0
lookinside11 <jxaqt009@qq.com>
2020-06-16 19:32:49 +0800

    3rdtyr

--- src/testRun1.java
+++ src/testRun1.java
@@ -3,7 +3,7 @@ import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class testRun {
+public class testRun1 {
     public static void main(String[] args) {
         //FactoryBean fb=new ClassPathXmlApplicationContext(("com/beans/beans.xml");
         //ApplicationContext ac=new ClassPathXmlApplicationContext("com/beans/beans.xml");
