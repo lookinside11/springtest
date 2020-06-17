ffba89d59484c2659924d46a0830928eb80f1274
lookinside11 <jxaqt009@qq.com>
2020-06-17 17:06:20 +0800

    OK

--- src/com/beans/Person.java
+++ src/com/beans/Person.java
@@ -3,16 +3,22 @@ package com.beans;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Service
+@Controller
+
 public class Person implements BeanNameAware, BeanFactoryAware, ApplicationContextAware,  InitializingBean, DisposableBean
 {
     public Person() {
--- src/testRun.java
+++ src/testRun.java
@@ -1,32 +1,28 @@
 import com.beans.Person;
+import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 
 
 public class testRun {
-    public static void main(String[] args) throws Exception {
 
-        //ConfigurableApplicationContext ac=new ClassPathXmlApplicationContext("com/beans/beans.xml");
-        //Person p1= (Person) ac.getBean("person");
-        //System.out.println(p1);
-        //p1.destroy();
-        //ac.registerShutdownHook();
+    public  void main1() throws Exception {
 
+    }
+
+    @Test
+    public void show() throws Exception {
         ClassPathXmlApplicationContext ac=new ClassPathXmlApplicationContext("com/beans/beans.xml");
         Person p1= ac.getBean("person",Person.class);
         p1.destroy();
-        p1=null;
-
         ac.close();
-
-
-
-
     }
 }
