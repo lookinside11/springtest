1cde90b3b454503095beb5461135cd8b0db7944d
lookinside11 <jxaqt009@qq.com>
2020-06-19 16:21:09 +0800

    OK

--- src/com/JDBCTemplate/BookImpl.java
+++ src/com/JDBCTemplate/BookImpl.java
@@ -1,8 +1,14 @@
 package com.JDBCTemplate;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class BookImpl implements BookDao{
 
+    //先把数据库模板搞进来
+    @Autowired
+    private JdbcTemplate jt;
 }
--- src/com/JDBCTemplate/BookService.java
+++ src/com/JDBCTemplate/BookService.java
@@ -1,8 +1,11 @@
 package com.JDBCTemplate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
+    @Autowired
+    private BookDao bd;
 }
